0966776eff274bdefb2707dbba2c9444b94c00d2
KingBael <www.jayesh567@gmail.com>
2022-10-27 12:36:14 +0530

    Updated

--- pages/signup.js
+++ pages/signup.js
@@ -11,10 +11,12 @@ const Signup = () => {
   const [Mail, setMail] = useState("");
   const [Phone, setPhone] = useState("");
   const [Branch, setBranch] = useState("NA");
+  const [RollNo, setRollNo] = useState("");
   const [Pass, setPass] = useState("");
   const [RePass, setRePass] = useState("");
 
   const [SignData, setSignData] = useState("");
+  const [stage, setStage] = useState(1);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,17 +25,6 @@ const Signup = () => {
       setPass("");
       setRePass("");
     } else {
-      // const tempData = {
-      //   Fname: FName,
-      //   LName: LName,
-      //   Gender: Gender,
-      //   DOB: DOB,
-      //   AdmissionData: AdmissionData,
-      //   Mail: Mail,
-      //   Phone: Phone,
-      //   Branch: Branch,
-      //   Pass: Pass
-      // };
       const tempData = {
         FName,
         LName,
@@ -43,29 +34,12 @@ const Signup = () => {
         Mail,
         Phone,
         Branch,
+        RollNo,
         Pass,
       };
       setSignData(tempData);
-      // console.log(tempData);
-
       sendData(tempData);
     }
-
-    // fetch("http://localhost:3000/api/userSave", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(SignData),
-    // })
-    //   .then((res) => res.text())
-    //   .then((demo) => {
-    //     console.log("Success: ", demo.toString());
-    //     alert("Submitted SuccessFully");
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error: ", error);
-    //   });
   };
 
   const genChange = (e) => {
@@ -87,8 +61,18 @@ const Signup = () => {
     })
       .then((res) => res.text())
       .then((demo) => {
-        console.log("Success: ", demo.toString());
+        // console.log("Success: ", demo.toString());
         alert("Submitted SuccessFully");
+        setFName("");
+        setLName("");
+        setGender("");
+        setDOB("");
+        setAdmissionData("");
+        setMail("");
+        setPhone("");
+        setBranch("");
+        setRollNo("");
+        setPass("");
       })
       .catch((error) => {
         console.error("Error: ", error);
@@ -101,160 +85,180 @@ const Signup = () => {
         <title>Sign Up</title>
       </Head>
       <div className={styles.wrapper}>
-        <div className={styles.heading}>Sign Up</div>
+        <div className={styles.heading}>Sign Up!</div>
         <div className={styles.innerform}>
           <form onSubmit={handleSubmit} className={styles.formBody}>
             <div className={styles.actualForm}>
-              <div className={styles.name}>
-                <label htmlFor="fname">Name</label>
+              <div className={styles.firstForm}>
+                <div className={styles.name}>
+                  <label htmlFor="fname">Name</label>
+                  <div>
+                    <input
+                      className={styles.input}
+                      type="text"
+                      placeholder="First Name"
+                      value={FName}
+                      // autocomplete="off"
+                      onChange={(event) => setFName(event.target.value)}
+                      // onChange={handleOnchange}
+                      required
+                    />
+                  </div>
+                  <div>
+                    <input
+                      className={styles.input}
+                      type="text"
+                      placeholder="Last Name"
+                      // autocomplete="off"
+                      value={LName}
+                      onChange={(event) => setLName(event.target.value)}
+                      // onChange={handleOnchange}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className={styles.flexBox}>
+                  Gender
+                  <div className={styles.flexWrap}>
+                    <div className={styles.intFlex}>
+                      <input
+                        className={styles.input}
+                        type="radio"
+                        value={"male"}
+                        onChange={genChange}
+                        // value={Gender.Male}
+                        name="gender"
+                        required
+                      />
+                      <label htmlFor="male">Male</label>
+                    </div>
+                    <div className={styles.intFlex}>
+                      <input
+                        className={styles.input}
+                        type="radio"
+                        value={"female"}
+                        onChange={genChange}
+                        // value={Gender.Female}
+                        name="gender"
+                        required
+                      />
+                      <label htmlFor="female">Female</label>
+                    </div>
+                  </div>
+                </div>
                 <div>
+                  <label htmlFor="DOB">D.O.B</label>
+                  <input
+                    className={[styles.dob, styles.input].join(" ")}
+                    type="date"
+                    onChange={(event) => setDOB(event.target.value)}
+                    value={DOB}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="admissionYear">Year of Admission</label>
+                  <input
+                    type="month"
+                    className={styles.input}
+                    value={AdmissionData}
+                    onChange={(event) => setAdmissionData(event.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    className={styles.input}
+                    type="email"
+                    pattern="[A-Za-z0-9.+-]+@+[A-Za-z0-9]+\.+[A-Za-z]{2,}"
+                    required
+                    placeholder="example@gmail.com"
+                    value={Mail}
+                    onChange={(event) => setMail(event.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="phone">Phone</label>
+                  <input
+                    className={styles.input}
+                    type="phone"
+                    required
+                    placeholder="10-digit Number"
+                    pattern="[0-9]{9,}"
+                    value={Phone}
+                    onChange={(event) => setPhone(event.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="branch">Branch</label>
+                  <select
+                    name="branch"
+                    className={[styles.input, styles.select].join(" ")}
+                    // value={Branch}
+                    onChange={(event) => setBranch(event.target.value)}
+                    value={Branch}
+                  >
+                    <option value={"NA"} disabled hidden>
+                      ------------------------------Select------------------------------
+                    </option>
+                    <option value={"Civil"}>Civil</option>
+                    <option value={"Mechanical"}>Mechanical</option>
+                    <option value={"Electrical"}>Electrical</option>
+                    <option value={"Electronics"}>Electronics</option>
+                    <option value={"CSE"}>Computer Science</option>
+                    <option value={"IT"}>Information Technology</option>
+                  </select>
+                </div>
+              </div>
+              <div className={styles.secondForm}>
+                <div>
+                  <label htmlFor="rollNo">Roll No.</label>
                   <input
                     className={styles.input}
                     type="text"
-                    placeholder="First Name"
-                    value={FName}
-                    onChange={(event) => setFName(event.target.value)}
-                    // onChange={handleOnchange}
+                    placeholder="BE20F05F063"
+                    value={RollNo}
+                    onChange={(event) => setRollNo(event.target.value)}
+                    pattern="[A-Z]{2}[0-9]{2}[A-Z]{1}[0-9]{2}[A-Z]{1}[0-9]{3}"
+                    title="Enter Roll Number"
                     required
                   />
                 </div>
-                <div>
-                  <input
-                    className={styles.input}
-                    type="text"
-                    placeholder="Last Name"
-                    value={LName}
-                    onChange={(event) => setLName(event.target.value)}
-                    // onChange={handleOnchange}
-                    required
-                  />
-                </div>
-              </div>
-              <div className={styles.flexBox}>
-                Gender
-                <div className={styles.intFlex}>
-                  <input
-                    className={styles.input}
-                    type="radio"
-                    value={"male"}
-                    onChange={genChange}
-                    // value={Gender.Male}
-                    name="gender"
-                    required
-                  />
-                  <label htmlFor="male">Male</label>
-                </div>
-                <div className={styles.intFlex}>
-                  <input
-                    className={styles.input}
-                    type="radio"
-                    value={"female"}
-                    onChange={genChange}
-                    // value={Gender.Female}
-                    name="gender"
-                    required
-                  />
-                  <label htmlFor="female">Female</label>
-                </div>
-              </div>
-              <div>
-                <label htmlFor="DOB">D.O.B</label>
-                <input
-                  className={[styles.dob, styles.input].join(" ")}
-                  type="date"
-                  onChange={(event) => setDOB(event.target.value)}
-                  value={DOB}
-                />
-              </div>
-              <div>
-                <label htmlFor="admissionYear">Year of Admission</label>
-                <input
-                  type="month"
-                  className={styles.input}
-                  value={AdmissionData}
-                  onChange={(event) => setAdmissionData(event.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="email">Email</label>
-                <input
-                  className={styles.input}
-                  type="email"
-                  pattern="[A-Za-z0-9.+-]+@+[A-Za-z0-9]+\.+[A-Za-z]{2,}"
-                  required
-                  placeholder="example@gmail.com"
-                  value={Mail}
-                  onChange={(event) => setMail(event.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="phone">Phone</label>
-                <input
-                  className={styles.input}
-                  type="phone"
-                  required
-                  placeholder="10-digit Number"
-                  pattern="[0-9]{9,}"
-                  value={Phone}
-                  onChange={(event) => setPhone(event.target.value)}
-                />
-              </div>
-              {/* <div>
-                <label htmlFor="rollNo">GID.</label>
-                <input className={styles.input}type="text" required />
-              </div> */}
-              <div>
-                <label htmlFor="branch">Branch</label>
-                <select
-                  name="branch"
-                  className={styles.input}
-                  // value={Branch}
-                  onChange={(event) => setBranch(event.target.value)}
-                  value={Branch}
-                >
-                  <option value={"NA"} disabled hidden>
-                    ------------------------------Select------------------------------
-                  </option>
-                  <option value={"Civil"}>Civil</option>
-                  <option value={"Mechanical"}>Mechanical</option>
-                  <option value={"Electrical"}>Electrical</option>
-                  <option value={"Electronics"}>Electronics</option>
-                  <option value={"CSE"}>Computer Science</option>
-                  <option value={"IT"}>Information Technology</option>
-                </select>
-              </div>
 
-              <div>
-                <label htmlFor="password">Create Password</label>
-                <input
-                  type="password"
-                  required
-                  minLength={8}
-                  title="Password Must be 8 Characters Long"
-                  className={styles.input}
-                  placeholder="******************"
-                  value={Pass}
-                  onChange={(event) => setPass(event.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="rePassword">RE-Password</label>
-                <input
-                  type="password"
-                  required
-                  minLength={8}
-                  title="Password Must be 8 Characters Long"
-                  className={styles.input}
-                  placeholder="******************"
-                  value={RePass}
-                  onChange={(event) => setRePass(event.target.value)}
-                />
+                <div>
+                  <label htmlFor="password">Create Password</label>
+                  <input
+                    type="password"
+                    required
+                    minLength={8}
+                    title="Password Must be 8 Characters Long"
+                    className={styles.input}
+                    placeholder="******************"
+                    value={Pass}
+                    onChange={(event) => setPass(event.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="rePassword">RE-Password</label>
+                  <input
+                    type="password"
+                    required
+                    minLength={8}
+                    title="Password Must be 8 Characters Long"
+                    className={styles.input}
+                    placeholder="******************"
+                    value={RePass}
+                    onChange={(event) => setRePass(event.target.value)}
+                  />
+                </div>
               </div>
             </div>
-            <div className={styles.submit}>
+            <div className={[styles.submit, styles.mainSubmit].join(" ")}>
               <button>Submit</button>
             </div>
           </form>
+          <div className={styles.submit}>
+            <button onClick={handleNext}>Next {"->"} </button>
+          </div>
         </div>
       </div>
     </div>
@@ -262,3 +266,7 @@ const Signup = () => {
 };
 
 export default Signup;
+
+const handleNext = () => {
+  console.log("first");
+};
