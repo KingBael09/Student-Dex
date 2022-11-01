6cbf7ab3293fea6ebe744c8423a09372236c6f2c
KingBael <www.jayesh567@gmail.com>
2022-11-01 17:44:37 +0530

    Added Functionality

--- pages/signup.js
+++ pages/signup.js
@@ -73,11 +73,226 @@ const Signup = () => {
         setBranch("");
         setRollNo("");
         setPass("");
+        setRePass("");
       })
       .catch((error) => {
         console.error("Error: ", error);
       });
   };
+
+  // Expremental Data Goes Here
+
+  // !Damn
+  const form = () => {
+    if (stage === 1) {
+      return (
+        <div className={styles.firstForm}>
+          <div className={styles.name}>
+            <label htmlFor="fname">Name</label>
+            <div>
+              <input
+                className={styles.input}
+                type="text"
+                placeholder="First Name"
+                value={FName}
+                // autocomplete="off"
+                onChange={(event) => setFName(event.target.value)}
+                // onChange={handleOnchange}
+                required
+              />
+            </div>
+            <div>
+              <input
+                className={styles.input}
+                type="text"
+                placeholder="Last Name"
+                // autocomplete="off"
+                value={LName}
+                onChange={(event) => setLName(event.target.value)}
+                // onChange={handleOnchange}
+                required
+              />
+            </div>
+          </div>
+          <div className={styles.flexBox}>
+            Gender
+            <div className={styles.flexWrap}>
+              <div className={styles.intFlex}>
+                <input
+                  className={styles.input}
+                  type="radio"
+                  value={"male"}
+                  onChange={genChange}
+                  // value={Gender.Male}
+                  name="gender"
+                  required
+                />
+                <label htmlFor="male">Male</label>
+              </div>
+              <div className={styles.intFlex}>
+                <input
+                  className={styles.input}
+                  type="radio"
+                  value={"female"}
+                  onChange={genChange}
+                  // value={Gender.Female}
+                  name="gender"
+                  required
+                />
+                <label htmlFor="female">Female</label>
+              </div>
+            </div>
+          </div>
+          <div>
+            <label htmlFor="DOB">D.O.B</label>
+            <input
+              className={[styles.dob, styles.input].join(" ")}
+              type="date"
+              onChange={(event) => setDOB(event.target.value)}
+              value={DOB}
+            />
+          </div>
+          <div>
+            <label htmlFor="admissionYear">Year of Admission</label>
+            <input
+              type="month"
+              className={styles.input}
+              value={AdmissionData}
+              onChange={(event) => setAdmissionData(event.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              className={styles.input}
+              type="email"
+              pattern="[A-Za-z0-9.+-]+@+[A-Za-z0-9]+\.+[A-Za-z]{2,}"
+              required
+              placeholder="example@gmail.com"
+              value={Mail}
+              onChange={(event) => setMail(event.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone</label>
+            <input
+              className={styles.input}
+              type="phone"
+              required
+              placeholder="10-digit Number"
+              pattern="[0-9]{9,}"
+              value={Phone}
+              onChange={(event) => setPhone(event.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="branch">Branch</label>
+            <select
+              name="branch"
+              className={[styles.input, styles.select].join(" ")}
+              // value={Branch}
+              onChange={(event) => setBranch(event.target.value)}
+              value={Branch}
+            >
+              <option value={"NA"} disabled hidden>
+                ------------------------------Select------------------------------
+              </option>
+              <option value={"Civil"}>Civil</option>
+              <option value={"Mechanical"}>Mechanical</option>
+              <option value={"Electrical"}>Electrical</option>
+              <option value={"Electronics"}>Electronics</option>
+              <option value={"CSE"}>Computer Science</option>
+              <option value={"IT"}>Information Technology</option>
+            </select>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.secondForm}>
+          <div>
+            <label htmlFor="rollNo">Roll No.</label>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="BE20F05F063"
+              value={RollNo}
+              onChange={(event) => setRollNo(event.target.value)}
+              pattern="[A-Z]{2}[0-9]{2}[A-Z]{1}[0-9]{2}[A-Z]{1}[0-9]{3}"
+              title="Enter Roll Number"
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password">Create Password</label>
+            <input
+              type="password"
+              required
+              minLength={8}
+              title="Password Must be 8 Characters Long"
+              className={styles.input}
+              placeholder="******************"
+              value={Pass}
+              onChange={(event) => setPass(event.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="rePassword">RE-Password</label>
+            <input
+              type="password"
+              required
+              minLength={8}
+              title="Password Must be 8 Characters Long"
+              className={styles.input}
+              placeholder="******************"
+              value={RePass}
+              onChange={(event) => setRePass(event.target.value)}
+            />
+          </div>
+        </div>
+      );
+    }
+  };
+
+  const submit = () => {
+    if (stage === 1) {
+      return (
+        <div className={styles.submit}>
+          <button onClick={handleNext}>Next {"->"} </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.submit}>
+          <button onClick={resetStage}>{"<-"} Back</button>
+        </div>
+      );
+    }
+  };
+
+  const submitMain = () => {
+    if (stage === 2) {
+      return (
+        <div className={[styles.submit, styles.mainSubmit].join(" ")}>
+          <button>Submit</button>
+        </div>
+      );
+    }
+  };
+
+  const resetStage = () => {
+    setStage(1);
+  };
+
+  // !Damn
+
+  const handleNext = () => {
+    // console.log("first");
+    setStage(2);
+  };
+
+  // Expremental Data Goes Here
 
   return (
     <div className={styles.container}>
@@ -89,7 +304,7 @@ const Signup = () => {
         <div className={styles.innerform}>
           <form onSubmit={handleSubmit} className={styles.formBody}>
             <div className={styles.actualForm}>
-              <div className={styles.firstForm}>
+              {/* <div className={styles.firstForm}>
                 <div className={styles.name}>
                   <label htmlFor="fname">Name</label>
                   <div>
@@ -208,8 +423,9 @@ const Signup = () => {
                     <option value={"IT"}>Information Technology</option>
                   </select>
                 </div>
-              </div>
-              <div className={styles.secondForm}>
+              </div> */}
+              {/* {FormOne()} */}
+              {/* <div className={styles.secondForm}>
                 <div>
                   <label htmlFor="rollNo">Roll No.</label>
                   <input
@@ -250,15 +466,19 @@ const Signup = () => {
                     onChange={(event) => setRePass(event.target.value)}
                   />
                 </div>
-              </div>
+              </div> */}
+              {/* {FormTwo()} */}
+              {form()}
             </div>
-            <div className={[styles.submit, styles.mainSubmit].join(" ")}>
+            {/* <div className={[styles.submit, styles.mainSubmit].join(" ")}>
               <button>Submit</button>
-            </div>
+            </div> */}
+            {submitMain()}
           </form>
-          <div className={styles.submit}>
+          {submit()}
+          {/* <div className={styles.submit}>
             <button onClick={handleNext}>Next {"->"} </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
@@ -266,7 +486,3 @@ const Signup = () => {
 };
 
 export default Signup;
-
-const handleNext = () => {
-  console.log("first");
-};
